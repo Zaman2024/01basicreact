9b8584c669d3d696d435776452fbe80d7b5be71f
Zaman <zamanofficial1980@gmail.com>
2024-03-25 00:32:54 +0530

    committed Some Changes

---
+++ src/Myp.js
@@ -0,0 +1,22 @@
+
+
+// function MyfirstReact() {
+//     return (
+//         <h1>hi My name is Zaman</h1>
+//     )
+
+
+// }
+
+// export default MyfirstReact;
+
+function MyRact() {
+    return (
+        <>
+            <h1> mr Zaman</h1>
+            <h3>  Hi My name is Khan and I'mnot a Terrorist</h3>
+        </>
+    );
+}
+
+export default MyRact;
